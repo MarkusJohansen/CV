283e1b225a0424467adf41baf0c26401625c05d7
Markus A. R. Johansen <90006516+MarkusJohansen@users.noreply.github.com>
2024-03-29 17:41:04 +0100

    Add styling and chimera dependencies

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <main class="main-wrapper">
+    <h1>Markus Johansen</h1>
+    <img src="./assets/markus.jpeg" alt="Markus Johansen" />
+  </main>
 </template>
+
+<script>
+import "./node_modules/chimeracss/build/chimera.css";
+import "./assets/style.css";
+</script>
